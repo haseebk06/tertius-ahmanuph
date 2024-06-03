37df9aee7e0a09c43eb9cfc8a4301d61c1d340da
Juniorawan06 <haseebawan.1560@gmail.com>
2024-06-04 04:33:04 +0500

    room cover

--- src/components/Pages/Room/Room.jsx
+++ src/components/Pages/Room/Room.jsx
@@ -1,19 +1,19 @@
-import './style.css'
-import room from './img/room.jpg'
-import { Container, Row, Col } from 'react-bootstrap'
+import "./style.css";
+import room from "./img/room.jpg";
+import { Container, Row, Col } from "react-bootstrap";
 
 const Room = () => {
   return (
-    <main id='room'>
-        <Container fluid>
-            <Row>
-                <Col>
-                    <img className='room-img' src={room} alt="room image"/>
-                </Col>
-            </Row>
-        </Container>
+    <main id="room">
+      <Container fluid>
+        <Row>
+          <Col>
+            <div className="room-img"></div>
+          </Col>
+        </Row>
+      </Container>
     </main>
-  )
-}
+  );
+};
 
-export default Room
+export default Room;
